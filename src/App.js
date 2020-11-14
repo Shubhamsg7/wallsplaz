1600b5b68fbdaa0248223b853cf5cf61fe706b30
Shubhamsg7 <shubhamkumar8028@gmail.com>
2020-11-14 09:31:01 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
 function App() {
+
+  const [value,setValue] = useState("")
+  const [results,setResults] = useState([])
+
+  const fetchImages = () => {
+    fetch(`https://api.unsplash.com/search/photos/?client_id=l6IQcyhiia9DEz2idn8TaSclBCMPMj9CE3r68Ai0AGY&query=${value}&orientation=squarish&per_page=12`)
+    .then(res=>res.json())
+    .then(data=>{
+      console.log(data)
+      setResults(data.results)
+    })
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="App">
+      <h1>WallSplaz<span style={{color:"red"}}>.</span></h1>
+        <div className="search">
+          <input type="text" value={value} onChange={(e)=>setValue(e.target.value)} placeholder="Search Anything" />
+          <button onClick={()=>fetchImages()}>Search</button>
+        </div>
+        <div className="gallery">
+          {
+            results.map((item)=>{
+              return <img className="item" key={item.id} src={item.urls.regular} />
+            })
+          }
+        </div>
+      </div>
+    </>
   );
 }
 
